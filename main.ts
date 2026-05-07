8de68544c3a2b13ec1e951f29c960dfd59db15d6
WissalJabaoui <wissal.jabaoui@stu.ocsb.ca>
2026-05-07 14:17:00 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
-game.splash("what is the diameter of the pizza?")
 let labour = 0.75
 let rent = 1
 let materials = 0.5
+let HST = 1.13
 let diameter_of_the_pizza = game.askForNumber("what is the diameter of the pizza?")
-let subtotal_of_the_pizza = labour + (rent + materials * diameter_of_the_pizza)
-game.splash(subtotal_of_the_pizza)
-let total_cost_of_the_pizza = subtotal_of_the_pizza * 1.13
-game.splash(total_cost_of_the_pizza)
+let subtotal_cost_of_the_pizza = labour + (rent + materials * diameter_of_the_pizza)
+let rounded_subtotal_cost = Math.round(subtotal_cost_of_the_pizza)
+game.splash("the rounded subtotal cost of the pizza is:" + rounded_subtotal_cost)
+let total_cost_of_the_pizza = subtotal_cost_of_the_pizza * HST
 let rounded_total_cost = Math.round(total_cost_of_the_pizza)
-game.splash(rounded_total_cost)
+game.splash("the rounded total cost of the pizza is:" + rounded_total_cost)
