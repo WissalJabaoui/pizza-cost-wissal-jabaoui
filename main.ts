f1257123f42acb16d6ecd2ac1d6c89cb966ab530
WissalJabaoui <wissal.jabaoui@stu.ocsb.ca>
2026-05-07 13:44:32 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+game.splash("what is the diameter of the pizza?")
+let labour = 0.75
+let rent = 1
+let materials = 0.5
+let diameter = 0
+let diameter_of_the_pizza = game.ask("what is the diameter of the pizza?")
+let subtotal_of_the_pizza = labour + (rent + materials * diameter)
